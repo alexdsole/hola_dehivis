a1d638bb559c6af39211112f4f5229de39e2616e
Alexis Sole <adavisole@gmail.com>
2017-03-16 13:53:14 -0400

    saludo dehivis

--- saludo/views.py
+++ saludo/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 def saludo(request):
-
-	return HttpResponse("hola deivis")
+	return render(request, 'saludo/saludo.html')
